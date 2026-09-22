aef684150dc504eb58a9ff3741ef8aaadc70918c
Ahmed <ahmed.khalid.elshamy37@gmail.com>
2026-09-23 00:18:41 +0300

    Implement User DAO CRUD operations

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -1,17 +1,26 @@
 package com.app;
 
-import com.app.util.DatabaseConnection;
+import com.app.dao.UserDAO;
+import com.app.model.User;
 
-import java.sql.Connection;
+import java.util.List;
 
 public class Main {
-    static void main(String[] args) {
-        try (
-                Connection connection = DatabaseConnection.getConnection();
-                ){
-            System.out.println("DataBase Connected");
-        } catch (Exception e){
-            e.printStackTrace();
+
+    public static void main(String[] args) {
+
+
+
+        UserDAO userDAO = new UserDAO();
+
+        List<User> users = userDAO.findAll();
+
+        for (User user : users) {
+            System.out.println(
+                    user.getId() + " | " +
+                            user.getUsername() + " | " +
+                            user.getEmail()
+            );
         }
     }
 }
--- src/main/java/com/app/dao/UserDAO.java
+++ src/main/java/com/app/dao/UserDAO.java
@@ -1,4 +1,174 @@
 package com.app.dao;
 
+import com.app.model.User;
+import com.app.util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDAO {
+
+
+    public boolean create(User user) {
+
+        String sql = """
+                INSERT INTO users (username, email)
+                VALUES (?, ?)
+                """;
+
+        try (
+                Connection connection =
+                        DatabaseConnection.getConnection();
+
+                PreparedStatement statement =
+                        connection.prepareStatement(sql)
+        ) {
+
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getEmail());
+
+            return statement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+
+
+
+
+    }
+
+
+    public List<User> findAll() {
+
+        String sql = """
+            SELECT * FROM users
+            """;
+
+        List<User> users = new ArrayList<>();
+
+        try (
+                Connection connection =
+                        DatabaseConnection.getConnection();
+
+                PreparedStatement statement =
+                        connection.prepareStatement(sql);
+
+                ResultSet resultSet =
+                        statement.executeQuery()
+        ) {
+
+            while (resultSet.next()) {
+
+                User user = new User(
+                        resultSet.getInt("id"),
+                        resultSet.getString("username"),
+                        resultSet.getString("email")
+                );
+
+                users.add(user);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
+    public User findById(int id) {
+
+        String sql = """
+            SELECT * FROM users
+            WHERE id = ?
+            """;
+
+        try (
+                Connection connection =
+                        DatabaseConnection.getConnection();
+
+                PreparedStatement statement =
+                        connection.prepareStatement(sql);
+
+                ResultSet resultSet =
+                        statement.executeQuery()
+        ) {
+
+            statement.setInt(1, id);
+
+            if (resultSet.next()) {
+
+                return new User(
+                        resultSet.getInt("id"),
+                        resultSet.getString("username"),
+                        resultSet.getString("email")
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public boolean update(User user) {
+
+        String sql = """
+            UPDATE users
+            SET username = ?, email = ?
+            WHERE id = ?
+            """;
+
+        try (
+                Connection connection =
+                        DatabaseConnection.getConnection();
+
+                PreparedStatement statement =
+                        connection.prepareStatement(sql)
+        ) {
+
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getEmail());
+            statement.setInt(3, user.getId());
+
+            return statement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean delete(int id) {
+
+        String sql = """
+            DELETE FROM users
+            WHERE id = ?
+            """;
+
+        try (
+                Connection connection =
+                        DatabaseConnection.getConnection();
+
+                PreparedStatement statement =
+                        connection.prepareStatement(sql)
+        ) {
+
+            statement.setInt(1, id);
+
+            return statement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
 }
